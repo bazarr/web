95b6f2f445348244dc6e6da84eefae80f05b7296
tasti <tasti@zakarie.com>
2016-06-26 12:37:53 -0700

    Add circle ci

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// eslint-disable-next-line react/prefer-stateless-function
 export default class App extends React.Component {
   render() {
     return <div>Hello, bazarr!</div>;
