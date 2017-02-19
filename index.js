888507352ca1c95f74be34048fa94d584afbdf0d
Manuel Sabu <manuelntu@yahoo.com.sg>
2017-02-18 19:50:16 -0500

    integrating basic group chat

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+'use strict';
+
 const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 9000;
+const http = require('http');
+const socket = require(`${__dirname}/routes/socket.js`);
+
+const server = http.createServer(app);
+var io = require('socket.io').listen(server);
+io.sockets.on('connection', socket);
+
 
 app.use('/public', express.static(`${__dirname}/public`));
 app.set('views', `${__dirname}/views`);
@@ -15,6 +24,12 @@ app.get('/search', (req,res) => {
   res.render('pages/search_results');
 });
 
-app.listen(PORT, () => {
+app.get('/chat', (req,res) => {
+  res.render('pages/chat_page');
+});
+
+server.listen(PORT, () => {
   console.log('Node app is running on port', PORT);
 });
+
+module.exports = app;
